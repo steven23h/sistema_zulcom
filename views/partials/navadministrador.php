3ab33f7dba521fa463adb66386b177477bf847b6
steven23h <steven.becerra@istvidanueva.edu.ec>
2026-05-14 16:35:15 -0500

    Correcion nav duplicado

--- views/partials/navadministrador.php
+++ views/partials/navadministrador.php
@@ -19,9 +19,7 @@
                 <a href="#">Clientes</a>
             </li>
 
-            <li class="nav-item"><a href="administrador.php">Dashboard</a></li>
-            <li class="nav-item"><a href="administrador.php?page=registrar">Registrar Personal</a></li>
-            <li class="nav-item"><a href="administrador.php?page=ver_planes">Planes</a></li>
+           
             <li class="nav-item"><a href="administrador.php?page=ver_clientes">Lista de Clientes</a></li>
             <li class="nav-item"><a href="administrador.php?page=ver_tickets">Soporte Técnico</a></li>
 

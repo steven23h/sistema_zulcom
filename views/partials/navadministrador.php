18cb2d74bbbc1819eb422d0e5283f2746f6275dc
steven23h <steven.becerra@istvidanueva.edu.ec>
2026-05-14 20:03:37 -0500

    crear rol en administrador

--- views/partials/navadministrador.php
+++ views/partials/navadministrador.php
@@ -1,28 +1,62 @@
 <aside class="sidebar">
+
     <div class="logo-container">
         <img class="img-logo" src="../../public/img/logo.png" alt="Logo Zulcom">
     </div>
 
     <div class="nav-section">
+
         <ul class="nav-list">
 
             <li class="nav-item">
-                <a href="administrador.php">Dashboard</a>
-            </li>
-            <li class="nav-item">
-                <a href="administrador.php?page=registrar">Registrar Personal</a>
-            </li>
-            <li class="nav-item">
-                <a href="administrador.php?page=ver_planes">Planes</a>
-            </li>
-            <li class="nav-item">
-                <a href="#">Clientes</a>
+                <a href="administrador.php">
+                    Dashboard
+                </a>
             </li>
 
-           
-            <li class="nav-item"><a href="administrador.php?page=ver_clientes">Lista de Clientes</a></li>
-            <li class="nav-item"><a href="administrador.php?page=ver_tickets">Soporte Técnico</a></li>
+            <li class="nav-item">
+                <a href="administrador.php?page=registrar">
+                    Registrar Personal
+                </a>
+            </li>
+
+            <li class="nav-item">
+                <a href="administrador.php?page=ver_planes">
+                    Planes
+                </a>
+            </li>
+
+            <li class="nav-item">
+                <a href="administrador.php?page=ver_clientes">
+                    Lista de Clientes
+                </a>
+            </li>
+
+            <li class="nav-item">
+                <a href="administrador.php?page=ver_tickets">
+                    Soporte Técnico
+                </a>
+            </li>
+
+
+            <li class="nav-item">
+                <a href="administrador.php?page=crear_rol">
+                    Crear Rol
+                </a>
+            </li>
+
+            <li class="nav-item">
+                <a href="administrador.php?page=ver_roles">
+                    Ver Roles
+                </a>
+            </li>
 
         </ul>
+
+        </li>
+
+        </ul>
+
     </div>
+
 </aside>

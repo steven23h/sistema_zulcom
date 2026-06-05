9aa612adb06a2758315b4ab329436cdda93db652
steven23h <steven.becerra@istvidanueva.edu.ec>
2026-06-05 07:41:15 -0500

    cambios

--- views/partials/navadministrador.php
+++ views/partials/navadministrador.php
@@ -1,41 +1,18 @@
 <aside class="sidebar">
-
     <div class="logo-container">
-
-        <img class="img-logo"
-            src="../../public/img/logo.png"
-            alt="Logo Zulcom">
-
+        <img class="img-logo" src="../../public/img/logo.png" alt="Logo Zulcom">
     </div>
 
     <div class="nav-section">
-
         <ul class="nav-list">
-
-            <li class="nav-item">
-                <a href="administrador.php">
-                    Dashboard
-                </a>
-            </li>
-
-            <li class="nav-item">
-                <a href="administrador.php?page=registrar">
-                    Registrar Personal
-                </a>
-            </li>
-
-            <li class="nav-item">
-                <a href="administrador.php?page=ver_planes">
-                    Planes
-                </a>
-            </li>
-
-            <li class="nav-item">
-                <a href="administrador.php?page=ver_clientes">
-                    Lista de Clientes
-                </a>
-            </li>
-
+            <li class="nav-item"><a href="administrador.php">Dashboard</a></li>
+            <li class="nav-item"><a href="administrador.php?page=registrar">Registrar Personal</a></li>
+            <li class="nav-item"><a href="administrador.php?page=ver_planes">Planes</a></li>
+            <li class="nav-item"><a href="administrador.php?page=ver_clientes">Lista de Clientes</a></li>
+            <li class="nav-item"><a href="administrador.php?page=ver_tickets">Soporte Técnico</a></li>
+             <li class="nav-item"><a href="administrador.php?page=crear_factura">Factura</a></li>
+             <li class="nav-item"><a href="administrador.php?page=clientes_deudores">deudores</a></li>
+             
             <li class="nav-item">
                 <a href="administrador.php?page=ver_roles">
                     Roles de Pago
@@ -47,33 +24,8 @@
                     Generar Rol
                 </a>
             </li>
-
-            <li class="nav-item">
-                <a href="administrador.php?page=ver_tickets">
-                    Soporte Técnico
-                </a>
-            </li>
-
-            <li class="nav-item">
-                <a href="administrador.php?page=crear_factura">
-                    Factura
-                </a>
-            </li>
-
-            <li class="nav-item">
-                <a href="administrador.php?page=clientes_deudores">
-                    Deudores
-                </a>
-            </li>
-
-            <li class="nav-item">
-                <a href="administrador.php?page=clientes_nodeudores">
-                    Pagados
-                </a>
-            </li>
-
+             <li class="nav-item"><a href="administrador.php?page=clientes_nodeudores">Pagados</a></li>
         </ul>
-
     </div>
-
 </aside>
+

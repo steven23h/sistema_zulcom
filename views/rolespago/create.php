ea524fafd5307726163319350f88052247ca522e
steven23h <steven.becerra@istvidanueva.edu.ec>
2026-06-10 19:45:41 -0500

    listo logo

--- views/rolespago/create.php
+++ views/rolespago/create.php
@@ -19,7 +19,7 @@ $colaboradores = $controller->listarColaboradores();
 <div class="dashboard-content">
   
    <link rel="stylesheet" href="../../public/css/navbar.css">
-  
+  <link rel="stylesheet" href="/zulcom2/css/styles.css">
     <!-- HEADER -->
     <div class="page-header">
         <div>
